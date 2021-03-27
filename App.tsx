9f57069bf9ff8f2f5b086da3b38f62374bda3248
lisichaviano <lpena@cimex.com.cu>
2021-03-26 21:07:09 -0400

    sum

--- App.tsx
+++ App.tsx
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View, TextInput, Text, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [plus, setPlus] = useState<any>(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.rowContainer}>
+          <Text>First number</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter Numeric Values Only"
+            placeholderTextColor="#60605e"
+            keyboardType={'numeric'}
+            onChangeText={(text) => { setFirst(Number(text)) }}
+          />
+        </View>
+        <View style={styles.rowContainer}>
+          <Text>Second number</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter Numeric Values Only"
+            placeholderTextColor="#60605e"
+            keyboardType={'numeric'}
+            onChangeText={(text) => { setSecond(Number(text)) }}
+          />
+        </View>
+        <View style={styles.rowContainer}>
+          <Button title="Sum" onPress={(ev) => setPlus(first + second)} />
+        </View>
+
+        <View style={styles.rowContainer}>
+          {plus !== null && (<Text>The sum is: {plus}</Text>)}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#ffffff',
+    flexDirection: "column",
+    padding: 16,
+    marginTop: 100,
   },
+  rowContainer: {
+    flexDirection: "column",
+    width: '100%',
+    marginBottom: 16
+  },
+
+  textInput: {
+    width: '100%',
+    backgroundColor: '#dde8c9',
+    padding: 16,
+  }, 
 });
